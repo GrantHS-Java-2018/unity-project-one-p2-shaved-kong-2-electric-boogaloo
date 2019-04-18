d7b22b5112499efe1f65e6bb3d0932c063fa8cb5
Jakson <jacksonnevin@gmail.com>
2019-04-18 12:10:48 -0700

    Start of Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform[] waypoints;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    [HideInInspector] public int waypointIndex = 0;
+
+    public bool moveAllowed = false;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (moveAllowed)
+            Move();
     }
 }
