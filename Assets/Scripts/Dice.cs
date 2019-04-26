4d9e99ce0b6241dc3e9629cb998a0b301c4d6e9e
josh <joshmulick@gmail.com>
2019-04-26 11:16:36 -0700

    thanksgiving STUFFing

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,43 +1,33 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    
     private Sprite[] diceSides;
-
     private SpriteRenderer rend;
-
     private int whosTurn = 1;
-
     private bool coroutineAllowed = true;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
         rend = GetComponent<SpriteRenderer>();
-
         diceSides = Resources.LoadAll<Sprite>("DiceSides/");
-
         rend.sprite = diceSides[5];
-
     }
 
     private void OnMouseDown()
     {
         if(!GameControl.gameOver && coroutineAllowed)
-        StartCoroutine("RollTheDice");
+             StartCoroutine("RollTheDice");
     }
 
     private IEnumerator RollTheDice()
     {
         coroutineAllowed = false;
        
-        var randomDiceSides = 0;
-        var finalSide = 0;
+        int randomDiceSides = 0;
+        int finalSide = 0;
         for (int i = 0; i <= 20; i++)
         {
             randomDiceSides = Random.Range(0, 5);
