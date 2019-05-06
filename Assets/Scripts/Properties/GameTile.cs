acc9af5010ba7e0c78f4ee1dfd836fca6f3d3db2
Shaved Kong <alindstedt03@gmail.com>
2019-05-06 10:39:47 -0700

    Some scripts for properties

---
+++ Assets/Scripts/Properties/GameTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    //street name
+    //street position
+    //street action
+
+    }
+}
---
+++ Assets/Scripts/Properties/Streets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streets : MonoBehaviour
+{
+    string[] streetArray = new string[22];
+    
+}
+
+public enum StreetType{
+    
+    NORMAL,
+    GO,
+    RAILROAD,
+    CHANCE,
+    COMMUNITY_CHEST,
+    GO_TO_JAIL,
+    JUST_VISITING,
+    UTILITY,
+    INCOME_TAX,
+    FREE_PARKING
+  
+}
