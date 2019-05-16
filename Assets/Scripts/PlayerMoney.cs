397cd1d7112d6171ca7064d66ca1a602f94f75f4
josh <joshmulick@gmail.com>
2019-05-16 11:34:09 -0700

    money

---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Money : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerMoney = 1500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
