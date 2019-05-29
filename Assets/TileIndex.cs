93bf051c0b4ee98addcf2b193209dd72bc897d99
Jakson <jacksonnevin@gmail.com>
2019-05-29 10:33:40 -0700

    Added tile index and created  c# class

---
+++ Assets/TileIndex.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Class for the tile index allowing purchase 
+public class TileIndex : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
